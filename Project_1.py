6aedb1743b8bba38635345040d5214b337ba0f28
KiriLink <jimiarti@hotmail.com>
2018-04-28 20:22:40 -0300

    Programa basico para calcular promedios

---
+++ Project_1.py
@@ -0,0 +1,504 @@
+#Autor: César Ignacio Aros Moraga
+#Fecha: 28/04/2018
+
+cont_notas = 0
+acu_notas = 0
+print("--------------------------------")
+limite = int(input("Ingrese la cantidad de notas: "))
+print("--------------------------------")
+while(True):
+    cont_notas = cont_notas + 1
+    notas = int(input("Ingrese una nota: "))
+    print("--------------------------------")
+    acu_notas = acu_notas + notas
+    if cont_notas == limite:
+        break
+
+promedio = acu_notas/ limite
+
+print("Su promedio final es:", promedio)
+print("--------------------------------")
+
+tecla = input("Si desea seguir usando el programa presione 1, de lo contrario presione cualquier tecla: ")
+
+if tecla == "1":
+    cont_notas = 0
+    acu_notas = 0
+    print("--------------------------------")
+    limite = int(input("Ingrese la cantidad de notas: "))
+    print("--------------------------------")
+    while (True):
+        cont_notas = cont_notas + 1
+        notas = int(input("Ingrese una nota: "))
+        print("--------------------------------")
+        acu_notas = acu_notas + notas
+        if cont_notas == limite:
+            break
+
+    promedio = acu_notas / limite
+
+    print("Su promedio final es:", promedio)
+    print("--------------------------------")
+else: raise SystemExit()
+tecla = input("Si desea seguir usando el programa presione 1, de lo contrario presione cualquier tecla: ")
+
+if tecla == "1":
+    cont_notas = 0
+    acu_notas = 0
+    print("--------------------------------")
+    limite = int(input("Ingrese la cantidad de notas: "))
+    print("--------------------------------")
+    while (True):
+        cont_notas = cont_notas + 1
+        notas = int(input("Ingrese una nota: "))
+        print("--------------------------------")
+        acu_notas = acu_notas + notas
+        if cont_notas == limite:
+            break
+
+    promedio = acu_notas / limite
+
+    print("Su promedio final es:", promedio)
+    print("--------------------------------")
+else: raise SystemExit()
+tecla = input("Si desea seguir usando el programa presione 1, de lo contrario presione cualquier tecla: ")
+
+if tecla == "1":
+    cont_notas = 0
+    acu_notas = 0
+    print("--------------------------------")
+    limite = int(input("Ingrese la cantidad de notas: "))
+    print("--------------------------------")
+    while (True):
+        cont_notas = cont_notas + 1
+        notas = int(input("Ingrese una nota: "))
+        print("--------------------------------")
+        acu_notas = acu_notas + notas
+        if cont_notas == limite:
+            break
+
+    promedio = acu_notas / limite
+
+    print("Su promedio final es:", promedio)
+    print("--------------------------------")
+else: raise SystemExit()
+tecla = input("Si desea seguir usando el programa presione 1, de lo contrario presione cualquier tecla: ")
+
+if tecla == "1":
+    cont_notas = 0
+    acu_notas = 0
+    print("--------------------------------")
+    limite = int(input("Ingrese la cantidad de notas: "))
+    print("--------------------------------")
+    while (True):
+        cont_notas = cont_notas + 1
+        notas = int(input("Ingrese una nota: "))
+        print("--------------------------------")
+        acu_notas = acu_notas + notas
+        if cont_notas == limite:
+            break
+
+    promedio = acu_notas / limite
+
+    print("Su promedio final es:", promedio)
+    print("--------------------------------")
+else: raise SystemExit()
+tecla = input("Si desea seguir usando el programa presione 1, de lo contrario presione cualquier tecla: ")
+
+if tecla == "1":
+    cont_notas = 0
+    acu_notas = 0
+    print("--------------------------------")
+    limite = int(input("Ingrese la cantidad de notas: "))
+    print("--------------------------------")
+    while (True):
+        cont_notas = cont_notas + 1
+        notas = int(input("Ingrese una nota: "))
+        print("--------------------------------")
+        acu_notas = acu_notas + notas
+        if cont_notas == limite:
+            break
+
+    promedio = acu_notas / limite
+
+    print("Su promedio final es:", promedio)
+    print("--------------------------------")
+else: raise SystemExit()
+tecla = input("Si desea seguir usando el programa presione 1, de lo contrario presione cualquier tecla: ")
+
+if tecla == "1":
+    cont_notas = 0
+    acu_notas = 0
+    print("--------------------------------")
+    limite = int(input("Ingrese la cantidad de notas: "))
+    print("--------------------------------")
+    while (True):
+        cont_notas = cont_notas + 1
+        notas = int(input("Ingrese una nota: "))
+        print("--------------------------------")
+        acu_notas = acu_notas + notas
+        if cont_notas == limite:
+            break
+
+    promedio = acu_notas / limite
+
+    print("Su promedio final es:", promedio)
+    print("--------------------------------")
+else: raise SystemExit()
+tecla = input("Si desea seguir usando el programa presione 1, de lo contrario presione cualquier tecla: ")
+
+if tecla == "1":
+    cont_notas = 0
+    acu_notas = 0
+    print("--------------------------------")
+    limite = int(input("Ingrese la cantidad de notas: "))
+    print("--------------------------------")
+    while (True):
+        cont_notas = cont_notas + 1
+        notas = int(input("Ingrese una nota: "))
+        print("--------------------------------")
+        acu_notas = acu_notas + notas
+        if cont_notas == limite:
+            break
+
+    promedio = acu_notas / limite
+
+    print("Su promedio final es:", promedio)
+    print("--------------------------------")
+else: raise SystemExit()
+tecla = input("Si desea seguir usando el programa presione 1, de lo contrario presione cualquier tecla: ")
+
+if tecla == "1":
+    cont_notas = 0
+    acu_notas = 0
+    print("--------------------------------")
+    limite = int(input("Ingrese la cantidad de notas: "))
+    print("--------------------------------")
+    while (True):
+        cont_notas = cont_notas + 1
+        notas = int(input("Ingrese una nota: "))
+        print("--------------------------------")
+        acu_notas = acu_notas + notas
+        if cont_notas == limite:
+            break
+
+    promedio = acu_notas / limite
+
+    print("Su promedio final es:", promedio)
+    print("--------------------------------")
+else: raise SystemExit()
+tecla = input("Si desea seguir usando el programa presione 1, de lo contrario presione cualquier tecla: ")
+
+if tecla == "1":
+    cont_notas = 0
+    acu_notas = 0
+    print("--------------------------------")
+    limite = int(input("Ingrese la cantidad de notas: "))
+    print("--------------------------------")
+    while (True):
+        cont_notas = cont_notas + 1
+        notas = int(input("Ingrese una nota: "))
+        print("--------------------------------")
+        acu_notas = acu_notas + notas
+        if cont_notas == limite:
+            break
+
+    promedio = acu_notas / limite
+
+    print("Su promedio final es:", promedio)
+    print("--------------------------------")
+else: raise SystemExit()
+tecla = input("Si desea seguir usando el programa presione 1, de lo contrario presione cualquier tecla: ")
+
+if tecla == "1":
+    cont_notas = 0
+    acu_notas = 0
+    print("--------------------------------")
+    limite = int(input("Ingrese la cantidad de notas: "))
+    print("--------------------------------")
+    while (True):
+        cont_notas = cont_notas + 1
+        notas = int(input("Ingrese una nota: "))
+        print("--------------------------------")
+        acu_notas = acu_notas + notas
+        if cont_notas == limite:
+            break
+
+    promedio = acu_notas / limite
+
+    print("Su promedio final es:", promedio)
+    print("--------------------------------")
+else: raise SystemExit()
+tecla = input("Si desea seguir usando el programa presione 1, de lo contrario presione cualquier tecla: ")
+
+if tecla == "1":
+    cont_notas = 0
+    acu_notas = 0
+    print("--------------------------------")
+    limite = int(input("Ingrese la cantidad de notas: "))
+    print("--------------------------------")
+    while (True):
+        cont_notas = cont_notas + 1
+        notas = int(input("Ingrese una nota: "))
+        print("--------------------------------")
+        acu_notas = acu_notas + notas
+        if cont_notas == limite:
+            break
+
+    promedio = acu_notas / limite
+
+    print("Su promedio final es:", promedio)
+    print("--------------------------------")
+else: raise SystemExit()
+tecla = input("Si desea seguir usando el programa presione 1, de lo contrario presione cualquier tecla: ")
+
+if tecla == "1":
+    cont_notas = 0
+    acu_notas = 0
+    print("--------------------------------")
+    limite = int(input("Ingrese la cantidad de notas: "))
+    print("--------------------------------")
+    while (True):
+        cont_notas = cont_notas + 1
+        notas = int(input("Ingrese una nota: "))
+        print("--------------------------------")
+        acu_notas = acu_notas + notas
+        if cont_notas == limite:
+            break
+
+    promedio = acu_notas / limite
+
+    print("Su promedio final es:", promedio)
+    print("--------------------------------")
+else: raise SystemExit()
+tecla = input("Si desea seguir usando el programa presione 1, de lo contrario presione cualquier tecla: ")
+
+if tecla == "1":
+    cont_notas = 0
+    acu_notas = 0
+    print("--------------------------------")
+    limite = int(input("Ingrese la cantidad de notas: "))
+    print("--------------------------------")
+    while (True):
+        cont_notas = cont_notas + 1
+        notas = int(input("Ingrese una nota: "))
+        print("--------------------------------")
+        acu_notas = acu_notas + notas
+        if cont_notas == limite:
+            break
+
+    promedio = acu_notas / limite
+
+    print("Su promedio final es:", promedio)
+    print("--------------------------------")
+else: raise SystemExit()
+tecla = input("Si desea seguir usando el programa presione 1, de lo contrario presione cualquier tecla: ")
+
+if tecla == "1":
+    cont_notas = 0
+    acu_notas = 0
+    print("--------------------------------")
+    limite = int(input("Ingrese la cantidad de notas: "))
+    print("--------------------------------")
+    while (True):
+        cont_notas = cont_notas + 1
+        notas = int(input("Ingrese una nota: "))
+        print("--------------------------------")
+        acu_notas = acu_notas + notas
+        if cont_notas == limite:
+            break
+
+    promedio = acu_notas / limite
+
+    print("Su promedio final es:", promedio)
+    print("--------------------------------")
+else: raise SystemExit()
+tecla = input("Si desea seguir usando el programa presione 1, de lo contrario presione cualquier tecla: ")
+
+if tecla == "1":
+    cont_notas = 0
+    acu_notas = 0
+    print("--------------------------------")
+    limite = int(input("Ingrese la cantidad de notas: "))
+    print("--------------------------------")
+    while (True):
+        cont_notas = cont_notas + 1
+        notas = int(input("Ingrese una nota: "))
+        print("--------------------------------")
+        acu_notas = acu_notas + notas
+        if cont_notas == limite:
+            break
+
+    promedio = acu_notas / limite
+
+    print("Su promedio final es:", promedio)
+    print("--------------------------------")
+else: raise SystemExit()
+tecla = input("Si desea seguir usando el programa presione 1, de lo contrario presione cualquier tecla: ")
+
+if tecla == "1":
+    cont_notas = 0
+    acu_notas = 0
+    print("--------------------------------")
+    limite = int(input("Ingrese la cantidad de notas: "))
+    print("--------------------------------")
+    while (True):
+        cont_notas = cont_notas + 1
+        notas = int(input("Ingrese una nota: "))
+        print("--------------------------------")
+        acu_notas = acu_notas + notas
+        if cont_notas == limite:
+            break
+
+    promedio = acu_notas / limite
+
+    print("Su promedio final es:", promedio)
+    print("--------------------------------")
+else: raise SystemExit()
+tecla = input("Si desea seguir usando el programa presione 1, de lo contrario presione cualquier tecla: ")
+
+if tecla == "1":
+    cont_notas = 0
+    acu_notas = 0
+    print("--------------------------------")
+    limite = int(input("Ingrese la cantidad de notas: "))
+    print("--------------------------------")
+    while (True):
+        cont_notas = cont_notas + 1
+        notas = int(input("Ingrese una nota: "))
+        print("--------------------------------")
+        acu_notas = acu_notas + notas
+        if cont_notas == limite:
+            break
+
+    promedio = acu_notas / limite
+
+    print("Su promedio final es:", promedio)
+    print("--------------------------------")
+else: raise SystemExit()
+tecla = input("Si desea seguir usando el programa presione 1, de lo contrario presione cualquier tecla: ")
+
+if tecla == "1":
+    cont_notas = 0
+    acu_notas = 0
+    print("--------------------------------")
+    limite = int(input("Ingrese la cantidad de notas: "))
+    print("--------------------------------")
+    while (True):
+        cont_notas = cont_notas + 1
+        notas = int(input("Ingrese una nota: "))
+        print("--------------------------------")
+        acu_notas = acu_notas + notas
+        if cont_notas == limite:
+            break
+
+    promedio = acu_notas / limite
+
+    print("Su promedio final es:", promedio)
+    print("--------------------------------")
+else: raise SystemExit()
+tecla = input("Si desea seguir usando el programa presione 1, de lo contrario presione cualquier tecla: ")
+
+if tecla == "1":
+    cont_notas = 0
+    acu_notas = 0
+    print("--------------------------------")
+    limite = int(input("Ingrese la cantidad de notas: "))
+    print("--------------------------------")
+    while (True):
+        cont_notas = cont_notas + 1
+        notas = int(input("Ingrese una nota: "))
+        print("--------------------------------")
+        acu_notas = acu_notas + notas
+        if cont_notas == limite:
+            break
+
+    promedio = acu_notas / limite
+
+    print("Su promedio final es:", promedio)
+    print("--------------------------------")
+else: raise SystemExit()
+tecla = input("Si desea seguir usando el programa presione 1, de lo contrario presione cualquier tecla: ")
+
+if tecla == "1":
+    cont_notas = 0
+    acu_notas = 0
+    print("--------------------------------")
+    limite = int(input("Ingrese la cantidad de notas: "))
+    print("--------------------------------")
+    while (True):
+        cont_notas = cont_notas + 1
+        notas = int(input("Ingrese una nota: "))
+        print("--------------------------------")
+        acu_notas = acu_notas + notas
+        if cont_notas == limite:
+            break
+
+    promedio = acu_notas / limite
+
+    print("Su promedio final es:", promedio)
+    print("--------------------------------")
+else: raise SystemExit()
+tecla = input("Si desea seguir usando el programa presione 1, de lo contrario presione cualquier tecla: ")
+
+if tecla == "1":
+    cont_notas = 0
+    acu_notas = 0
+    print("--------------------------------")
+    limite = int(input("Ingrese la cantidad de notas: "))
+    print("--------------------------------")
+    while (True):
+        cont_notas = cont_notas + 1
+        notas = int(input("Ingrese una nota: "))
+        print("--------------------------------")
+        acu_notas = acu_notas + notas
+        if cont_notas == limite:
+            break
+
+    promedio = acu_notas / limite
+
+    print("Su promedio final es:", promedio)
+    print("--------------------------------")
+else: raise SystemExit()
+tecla = input("Si desea seguir usando el programa presione 1, de lo contrario presione cualquier tecla: ")
+
+if tecla == "1":
+    cont_notas = 0
+    acu_notas = 0
+    print("--------------------------------")
+    limite = int(input("Ingrese la cantidad de notas: "))
+    print("--------------------------------")
+    while (True):
+        cont_notas = cont_notas + 1
+        notas = int(input("Ingrese una nota: "))
+        print("--------------------------------")
+        acu_notas = acu_notas + notas
+        if cont_notas == limite:
+            break
+
+    promedio = acu_notas / limite
+
+    print("Su promedio final es:", promedio)
+    print("--------------------------------")
+else: raise SystemExit()
+tecla = input("Si desea seguir usando el programa presione 1, de lo contrario presione cualquier tecla: ")
+
+if tecla == "1":
+    cont_notas = 0
+    acu_notas = 0
+    print("--------------------------------")
+    limite = int(input("Ingrese la cantidad de notas: "))
+    print("--------------------------------")
+    while (True):
+        cont_notas = cont_notas + 1
+        notas = int(input("Ingrese una nota: "))
+        print("--------------------------------")
+        acu_notas = acu_notas + notas
+        if cont_notas == limite:
+            break
+
+    promedio = acu_notas / limite
+
+    print("Su promedio final es:", promedio)
+    print("--------------------------------")
+else: raise SystemExit()
